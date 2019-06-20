1f41a11af35f54393bd69e3f055066efc120dacb
Damini Varu <damvaru4@gmail.com>
2019-06-20 02:23:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var recipes = {}
+var recipes = {};
+
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {[key]: value})
+    var newObj = Object.assign({}, object);
+    newObj[key] = value;
+    return newObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+    object[key] = value;
+    return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({}, object)
-  delete newObject[key]
-  return newObject
+    var newObj = Object.assign({}, object);
+    delete newObj[key];
+    return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
-  return object
+    delete object[key];
+    return object;
 }
